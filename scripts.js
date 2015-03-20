80a5c68ce40738ed72ddff8dca0b20bcf2b8c555
njs236 <njs236@student.cpit.ac.nz>
2015-03-20 14:59:06 +1300

    implementation of hiding screens
    using divs for table rather than table. it produces better results. has
    more control over the position of objects on the page.

--- scripts.js
+++ scripts.js
@@ -2,3 +2,23 @@
 
 var LoadGameData = function () {
 };
+
+var hideAllPages = function () {
+		"use strict";
+		console.groupCollapsed("viewModule : hideAll()");
+		var screens = [],
+			count;
+		screens = document.getElementsByClassName("navScreen");
+		for ( count = 0; count < screens.length; count = count + 1 ) {
+			console.log("hideAll() : Hiding " + screens[count].id);
+			screens[count].hidden = true;
+		};
+		console.groupEnd();
+	}
+
+var showScreen = function (id) {
+    var screen = document.getElementById(id);
+    hideAllPages()
+   screen.hidden = false;
+}
+
